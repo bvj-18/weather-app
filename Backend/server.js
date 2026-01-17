43eb4b21c029452cc7b3895844a4c1a7b022a14f
bvj-18 <bvishwajananii@gmail.com>
2026-01-17 13:13:28 +0530

    backend weather api integration

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const fetch = require("node-fetch");
 
+console.log("ENV API Key:", process.env.WEATHER_API_KEY);
+
 const app = express();
 const PORT = 5000;
 
@@ -17,8 +19,12 @@ app.get("/weather", async (req, res) => {
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=metric`;
 
+  console.log("Request URL:", url);
+
   const response = await fetch(url);
   const data = await response.json();
+
+  console.log("API Response:", data);
 
   res.json(data);
 });
